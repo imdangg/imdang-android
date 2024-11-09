36daae0ff37d0448288dc33a2ed73b4096764d0b
김한식 <dev.imaec@gmail.com>
2024-11-10 02:35:30 +0900

    chore: 빌드 구성 수정

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -25,7 +25,7 @@ android {
             )
         }
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
--- remote/build.gradle.kts
+++ remote/build.gradle.kts
@@ -23,7 +23,7 @@ android {
             )
         }
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
