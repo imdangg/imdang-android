c41d746a7d125ba28560eeb46a7badd4d2f7633c
juseong <wjdwntjd55@naver.com>
2024-11-14 21:37:47 +0900

    feat : 가입 완료 UI 구현

--- app/src/main/java/info/imdang/imdang/ui/basicinformation/BasicInformationActivity.kt
+++ app/src/main/java/info/imdang/imdang/ui/basicinformation/BasicInformationActivity.kt
@@ -4,11 +4,23 @@ import android.os.Bundle
 import dagger.hilt.android.AndroidEntryPoint
 import info.imdang.imdang.R
 import info.imdang.imdang.base.BaseActivity
+import info.imdang.imdang.common.ext.startActivity
 import info.imdang.imdang.databinding.ActivityBasicInformationBinding
+import info.imdang.imdang.ui.joincomplete.JoinCompleteActivity
 
 @AndroidEntryPoint
 class BasicInformationActivity : BaseActivity<ActivityBasicInformationBinding>(R.layout.activity_basic_information) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        init()
+    }
+
+    private fun init() {
+        with(binding) {
+            btnComplete.setOnClickListener {
+                startActivity<JoinCompleteActivity>()
+            }
+        }
     }
 }
