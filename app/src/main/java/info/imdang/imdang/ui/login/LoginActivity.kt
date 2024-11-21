e4505aa9d1b8f417eb35c9bca07169da86528ee6
김한식 <dev.imaec@gmail.com>
2024-11-21 16:30:35 +0900

    chore: Github Actions, fastlane 연동

--- app/src/main/java/info/imdang/imdang/ui/login/LoginActivity.kt
+++ app/src/main/java/info/imdang/imdang/ui/login/LoginActivity.kt
@@ -58,6 +58,8 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login
                     }
                 )
             }
+        } else {
+            Toast.makeText(this, "구글 로그인에 실패 했습니다.", Toast.LENGTH_SHORT).show()
         }
     }
 
