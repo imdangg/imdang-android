d8d14d05e5e8ba9e36b8eb6cf1c2bca2e8c487ac
juseong <wjdwntjd55@naver.com>
2024-12-16 22:14:57 +0900

    chore : MIN_SDK 28 -> 29 로 변경

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -1,6 +1,6 @@
 object AppConfig {
     const val COMPILE_SDK = 34
-    const val MIN_SDK = 28
+    const val MIN_SDK = 29
     const val TARGET_SDK = 34
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0.0"
