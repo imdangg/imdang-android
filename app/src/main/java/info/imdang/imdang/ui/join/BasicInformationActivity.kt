89e0a012748231564c32b3fb808eed9bf334245c
juseong <wjdwntjd55@naver.com>
2024-12-16 22:15:31 +0900

    feat : 기본정보입력시 오류 체크하는 기능 구현

--- app/src/main/java/info/imdang/imdang/ui/join/BasicInformationActivity.kt
+++ app/src/main/java/info/imdang/imdang/ui/join/BasicInformationActivity.kt
@@ -1,17 +1,25 @@
 package info.imdang.imdang.ui.join
 
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import info.imdang.imdang.R
 import info.imdang.imdang.base.BaseActivity
+import info.imdang.imdang.common.ext.birthDateValidation
 import info.imdang.imdang.common.ext.hideKeyboard
+import info.imdang.imdang.common.ext.nicknameValidation
 import info.imdang.imdang.common.ext.setMargin
 import info.imdang.imdang.common.ext.startAndFinishActivity
 import info.imdang.imdang.databinding.ActivityBasicInformationBinding
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class BasicInformationActivity :
     BaseActivity<ActivityBasicInformationBinding>(R.layout.activity_basic_information) {
+
+    private val basicInformationViewModel by viewModels<BasicInformationViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,9 +32,7 @@ class BasicInformationActivity :
 
         with(binding.btnComplete) {
             setMargin(left = 0, right = 0, top = 0, bottom = 0)
-            setBackgroundResource(info.imdang.component.R.color.orange_500)
             text = getString(info.imdang.component.R.string.confirm)
-            setTextColor(getColor(info.imdang.component.R.color.white))
         }
     }
 
@@ -35,25 +41,187 @@ class BasicInformationActivity :
 
         with(binding.btnComplete) {
             setMargin(left = 20, right = 20, top = 0, bottom = 40)
-            setBackgroundResource(info.imdang.component.R.drawable.bg_complete_button)
             text = getString(info.imdang.component.R.string.complete)
-            setTextColor(getColor(info.imdang.component.R.color.gray_500))
+            binding.etNickName.clearFocus()
+            binding.etBirthDate.clearFocus()
         }
     }
 
     private fun init() {
         with(binding) {
+            viewModel = basicInformationViewModel
             ivBack.setOnClickListener {
                 finish()
             }
+
+            etNickName.nicknameValidation(
+                maxLength = 10,
+                targetTextView = tvNicknameCount,
+                errorTextView = tvNicknameError,
+                statusImageView = ivNickName,
+                invalidIconResId = info.imdang.component.R.drawable.ic_exclamation_mark_red,
+                parentLayout = tlNickname,
+                onValidStateChanged = { isValid ->
+                    basicInformationViewModel.updateNicknameValid(isValid)
+                    if (!isValid) {
+                        basicInformationViewModel.updateFinalNickNameValid(false)
+                    }
+                }
+            )
+            etBirthDate.birthDateValidation(
+                errorTextView = tvBirthDateError,
+                statusImageView = ivBirthDate,
+                invalidIconResId = info.imdang.component.R.drawable.ic_exclamation_mark_red,
+                parentLayout = tlBirthDate,
+                onValidStateChanged = { isValid ->
+                    basicInformationViewModel.updateBirthDateValid(isValid)
+                    if (!isValid) {
+                        basicInformationViewModel.updateFinalBirthDateValid(false)
+                    }
+                }
+            )
+
+            etNickName.setOnFocusChangeListener { _, hasFocus ->
+                if (hasFocus) {
+                    lifecycleScope.launch {
+                        basicInformationViewModel.isNicknameValid.collect { isValid ->
+                            btnComplete.isEnabled = isValid
+                            checkButtonEnabled()
+                        }
+                    }
+                } else {
+                    lifecycleScope.launch {
+                        basicInformationViewModel.isFinalButtonEnabled.collect { isValid ->
+                            btnComplete.isEnabled = isValid
+                            checkButtonEnabled()
+                        }
+                    }
+                }
+            }
+
+            etBirthDate.setOnFocusChangeListener { _, hasFocus ->
+                if (hasFocus) {
+                    lifecycleScope.launch {
+                        basicInformationViewModel.isBirthDateValid.collect { isValid ->
+                            btnComplete.isEnabled = isValid
+                            checkButtonEnabled()
+                        }
+                    }
+                } else {
+                    lifecycleScope.launch {
+                        basicInformationViewModel.isFinalButtonEnabled.collect { isValid ->
+                            btnComplete.isEnabled = isValid
+                            checkButtonEnabled()
+                        }
+                    }
+                }
+            }
+
+            btnGenderMan.setOnClickListener {
+                updateGenderSelection(true)
+            }
+
+            btnGenderWoman.setOnClickListener {
+                updateGenderSelection(false)
+            }
+
+            lifecycleScope.launch {
+                basicInformationViewModel.isFinalButtonEnabled.collect { isValid ->
+                    btnComplete.isEnabled = isValid
+                    checkButtonEnabled()
+                }
+            }
+
             btnComplete.setOnClickListener {
                 if (isVisibleKeyboard) {
-                    hideKeyboard()
+                    when {
+                        etNickName.isFocused -> {
+                            val isNicknameValid = basicInformationViewModel.isNicknameValid.value
+
+                            if (isNicknameValid) {
+                                ivNickName.setImageResource(
+                                    info.imdang.component.R.drawable.ic_check
+                                )
+                                basicInformationViewModel.updateFinalNickNameValid(true)
+                                hideKeyboard()
+                            } else {
+                                ivNickName.setImageResource(
+                                    info.imdang.component.R.drawable.ic_exclamation_mark_red
+                                )
+                                basicInformationViewModel.updateFinalNickNameValid(false)
+                            }
+                        }
+
+                        etBirthDate.isFocused -> {
+                            val isBirthDateValid = basicInformationViewModel.isBirthDateValid.value
+                            if (isBirthDateValid) {
+                                ivBirthDate.setImageResource(
+                                    info.imdang.component.R.drawable.ic_check
+                                )
+                                basicInformationViewModel.updateFinalBirthDateValid(true)
+                                hideKeyboard()
+                            } else {
+                                ivBirthDate.setImageResource(
+                                    info.imdang.component.R.drawable.ic_exclamation_mark_red
+                                )
+                                basicInformationViewModel.updateFinalBirthDateValid(false)
+                            }
+                        }
+                    }
                 } else {
                     setResult(RESULT_OK)
                     startAndFinishActivity<JoinCompleteActivity>()
                 }
             }
+        }
+    }
+
+    private fun checkButtonEnabled() {
+        binding.btnComplete.setTextColor(
+            if (binding.btnComplete.isEnabled) {
+                getColor(info.imdang.component.R.color.white)
+            } else {
+                getColor(info.imdang.component.R.color.gray_500)
+            }
+        )
+    }
+
+    private fun updateGenderSelection(isManSelected: Boolean) {
+        with(binding) {
+            btnGenderMan.background = ContextCompat.getDrawable(
+                this@BasicInformationActivity,
+                if (isManSelected) {
+                    info.imdang.component.R.drawable.sr_orange50_orange500_all8
+                } else {
+                    info.imdang.component.R.drawable.sr_white_gray100_all8
+                }
+            )
+            btnGenderMan.setTextColor(
+                if (isManSelected) {
+                    getColor(info.imdang.component.R.color.orange_500)
+                } else {
+                    getColor(info.imdang.component.R.color.gray_400)
+                }
+            )
+
+            btnGenderWoman.background = ContextCompat.getDrawable(
+                this@BasicInformationActivity,
+                if (!isManSelected) {
+                    info.imdang.component.R.drawable.sr_orange50_orange500_all8
+                } else {
+                    info.imdang.component.R.drawable.sr_white_gray100_all8
+                }
+            )
+            btnGenderWoman.setTextColor(
+                if (!isManSelected) {
+                    getColor(info.imdang.component.R.color.orange_500)
+                } else {
+                    getColor(info.imdang.component.R.color.gray_400)
+                }
+            )
+
+            ivGender.setImageResource(info.imdang.component.R.drawable.ic_check)
+            basicInformationViewModel.updateFinalGenderValid(true)
         }
     }
 }
