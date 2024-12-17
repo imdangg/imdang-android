4eba11417a82e9344292d666aca567b7b6d507c3
김한식 <dev.imaec@gmail.com>
2024-12-17 16:55:08 +0900

    feat: 인사이트 상세 화면 UI 구현

--- app/src/main/java/info/imdang/imdang/common/bindingadapter/ChipBindingAdapter.kt
+++ app/src/main/java/info/imdang/imdang/common/bindingadapter/ChipBindingAdapter.kt
@@ -1,7 +1,46 @@
 package info.imdang.imdang.common.bindingadapter
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
 import androidx.databinding.BindingAdapter
 import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+import info.imdang.imdang.R
+
+@BindingAdapter("bindInsightDetailChips")
+fun ChipGroup.bindInsightDetailChips(chips: List<String>) {
+    removeAllViews()
+    chips.forEach {
+        bindInsightDetailChip(it, false)
+    }
+}
+
+@BindingAdapter(
+    value = ["bindInsightDetailChip", "bindIsRemoveAllView"],
+    requireAll = false
+)
+fun ChipGroup.bindInsightDetailChip(chip: String, isRemoveAllView: Boolean) {
+    if (isRemoveAllView) removeAllViews()
+    addView(createInsightDetailChip(context, chip))
+}
+
+@SuppressLint("InflateParams")
+private fun createInsightDetailChip(context: Context, text: String): Chip {
+    val chip = LayoutInflater
+        .from(context)
+        .inflate(R.layout.chip_insight_detail, null, false) as Chip
+    return chip.apply {
+        this.text = text
+        if (text == "해당 없음" || text == "잘모르겠어요") {
+            setTextColor(context.getColor(info.imdang.component.R.color.gray_500))
+            setChipStrokeColorResource(info.imdang.component.R.color.gray_100)
+            chipBackgroundColor = android.content.res.ColorStateList.valueOf(
+                context.getColor(info.imdang.component.R.color.gray_50)
+            )
+        }
+    }
+}
 
 @BindingAdapter("bindChipBackground")
 fun Chip.bindChipBackground(isSelected: Boolean) {
