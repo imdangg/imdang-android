b2d591b13589ba1276dbc4214006a0b40b5a02e2
김한식 <dev.imaec@gmail.com>
2024-12-19 18:48:12 +0900

    chore: 네이버 맵 API ClientId 변경

--- buildSrc/src/main/java/DevConfig.kt
+++ buildSrc/src/main/java/DevConfig.kt
@@ -3,5 +3,5 @@ object DevConfig {
     const val KAKAO_NATIVE_KEY = "59e9eec5c9f86687f9bf55b7c251dc76"
     const val GOOGLE_WEB_CLIENT_ID =
         "999413225080-k2rugvbe1sn71gr8qka5n2jmuq7u692v.apps.googleusercontent.com"
-    const val NAVER_CLIENT_ID = "l9b2u6ba8p"
+    const val NAVER_CLIENT_ID = "pdh1r07b6j"
 }
