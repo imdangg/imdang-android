f174743f4ee74464a93181d87f3ec0c05a35c03b
김한식 <dev.imaec@gmail.com>
2025-01-02 16:38:34 +0900

    feat: 인사이트 상세 교환 팝업 구현

--- app/src/main/java/info/imdang/imdang/ui/insight/InsightDetailViewModel.kt
+++ app/src/main/java/info/imdang/imdang/ui/insight/InsightDetailViewModel.kt
@@ -6,8 +6,11 @@ import info.imdang.imdang.base.BaseViewModel
 import info.imdang.imdang.model.insight.InsightDetailItem
 import info.imdang.imdang.model.insight.InsightDetailState
 import info.imdang.imdang.model.insight.InsightDetailVo
+import info.imdang.imdang.model.insight.InsightVo
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -15,11 +18,17 @@ import javax.inject.Inject
 @HiltViewModel
 class InsightDetailViewModel @Inject constructor() : BaseViewModel() {
 
+    private val _event = MutableSharedFlow<InsightDetailEvent>()
+    val event = _event.asSharedFlow()
+
     private val _insight = MutableStateFlow(InsightDetailVo.getSample())
     val insight = _insight.asStateFlow()
 
     private val _insightDetails = MutableStateFlow<List<InsightDetailItem>>(emptyList())
     val insightDetails = _insightDetails.asStateFlow()
+
+    private val _myInsights = MutableStateFlow<List<InsightVo>>(emptyList())
+    val myInsights = _myInsights.asStateFlow()
 
     private val _insightDetailState = MutableStateFlow(InsightDetailState.ExchangeRequest)
     val insightDetailState = _insightDetailState.asStateFlow()
@@ -43,6 +52,7 @@ class InsightDetailViewModel @Inject constructor() : BaseViewModel() {
                     InsightDetailItem.Invisible(insightDetailState.value)
                 )
             }
+        _myInsights.value = InsightVo.getSamples(10)
     }
 
     fun isEnableTabMove() = insightDetailState.value == InsightDetailState.ExchangeComplete
@@ -60,30 +70,56 @@ class InsightDetailViewModel @Inject constructor() : BaseViewModel() {
     }
 
     fun onClickExchangeRequestButton() {
-        // todo : 교환 요청, 아래 코드는 테스트용
-        _insightDetailState.value = InsightDetailState.ExchangeRequested
-        _insightDetails.value = insightDetails.value.map {
-            if (it is InsightDetailItem.Invisible) it.copy(insightDetailState.value) else it
+        viewModelScope.launch {
+            _event.emit(InsightDetailEvent.ShowMyInsightsBottomSheet)
         }
     }
 
     fun onClickRejectButton() {
-        // todo : 교환 거절, 아래 코드는 테스트용
-        _insightDetailState.value = InsightDetailState.ExchangeWaiting
-        _insightDetails.value = insightDetails.value.map {
-            if (it is InsightDetailItem.Invisible) it.copy(insightDetailState.value) else it
+        viewModelScope.launch {
+            _event.emit(
+                InsightDetailEvent.ShowExchangeDialog(message = "교환을 거절했어요.")
+            )
         }
     }
 
     fun onClickAcceptButton() {
-        // todo : 교환 수락, 아래 코드는 테스트용
-        _insightDetailState.value = InsightDetailState.ExchangeComplete
-        _insightDetails.value = listOf(
-            InsightDetailItem.BasicInfo(insight.value.basicInfo),
-            InsightDetailItem.Infra(insight.value.infra),
-            InsightDetailItem.AptEnvironment(insight.value.aptEnvironment),
-            InsightDetailItem.AptFacility(insight.value.aptFacility),
-            InsightDetailItem.GoodNews(insight.value.goodNews)
-        )
+        viewModelScope.launch {
+            _insightDetailState.value = InsightDetailState.ExchangeComplete
+            _insightDetails.value = listOf(
+                InsightDetailItem.BasicInfo(insight.value.basicInfo),
+                InsightDetailItem.Infra(insight.value.infra),
+                InsightDetailItem.AptEnvironment(insight.value.aptEnvironment),
+                InsightDetailItem.AptFacility(insight.value.aptFacility),
+                InsightDetailItem.GoodNews(insight.value.goodNews)
+            )
+            _event.emit(
+                InsightDetailEvent.ShowExchangeDialog(
+                    message = "교환 요청을 수락했어요.\n보관함에서 확인해보세요.",
+                    checkButtonText = "보관함 확인하기"
+                )
+            )
+        }
+    }
+
+    fun requestExchange() {
+        viewModelScope.launch {
+            _insightDetailState.value = InsightDetailState.ExchangeWaiting
+            _insightDetails.value = insightDetails.value.map {
+                if (it is InsightDetailItem.Invisible) it.copy(insightDetailState.value) else it
+            }
+            _event.emit(
+                InsightDetailEvent.ShowExchangeDialog(
+                    message = "교환 요청을 완료했어요.\n교환 내역은 교환소에서 확인해보세요.",
+                    checkButtonText = "교환소 확인하기"
+                )
+            )
+        }
+    }
+
+    fun onClickMyInsight(myInsight: InsightVo) {
+        _myInsights.value = myInsights.value.map {
+            it.copy(isSelected = it.insightId == myInsight.insightId)
+        }
     }
 }
