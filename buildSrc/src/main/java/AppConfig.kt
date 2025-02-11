f2e466e9e1ca73d2bf4a76acfe3115bc90df8431
김한식 <dev.imaec@gmail.com>
2025-02-11 22:24:05 +0900

    chore: 1.0.0(7) 버전 배포

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -2,6 +2,6 @@ object AppConfig {
     const val COMPILE_SDK = 34
     const val MIN_SDK = 29
     const val TARGET_SDK = 34
-    const val VERSION_CODE = 6
+    const val VERSION_CODE = 7
     const val VERSION_NAME = "1.0.0"
 }
