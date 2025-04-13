b145683fa53ba5531b69c6cd8b1444fba25005a7
juseong <wjdwntjd55@naver.com>
2025-04-13 23:24:22 +0900

    chore: 1.0.0(9) 버전 배포

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -2,6 +2,6 @@ object AppConfig {
     const val COMPILE_SDK = 34
     const val MIN_SDK = 29
     const val TARGET_SDK = 34
-    const val VERSION_CODE = 8
+    const val VERSION_CODE = 9
     const val VERSION_NAME = "1.0.0"
 }
